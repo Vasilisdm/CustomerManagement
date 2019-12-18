ef1f4c9fcb3c2448c344f73c2a8d552100cdb013
vasilisdm <vasilisdmr@gmail.com>
2019-12-18 20:51:01 +0200

    Added the necessary properties to customer class.

--- CustomerManagement/Customer.cs
+++ CustomerManagement/Customer.cs
@@ -4,6 +4,16 @@ namespace CustomerManagement
 {
     public class Customer
     {
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public int CustomerId { get; set; }
+        public string Email { get; set; }
         public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
