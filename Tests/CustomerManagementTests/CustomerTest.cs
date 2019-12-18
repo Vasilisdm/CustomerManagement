4f4a884d0e702094bfd0e216eb21d8b38d497ace
vasilisdm <vasilisdmr@gmail.com>
2019-12-18 21:03:39 +0200

    Created a test that checks the functionality of the full name property.

---
+++ Tests/CustomerManagementTests/CustomerTest.cs
@@ -0,0 +1,27 @@
+using CustomerManagement;
+using NUnit.Framework;
+
+namespace CustomerManagementTests
+{
+    public class CustomerTest
+    {
+        [Test]
+        public void FullNameTestValid()
+        {
+            // Arrange
+            Customer customer = new Customer()
+            {
+                FirstName = "Frodo",
+                LastName = "Baggins"
+            };
+
+            string excpectedFullName = "Frodo Baggins";
+
+            // Act
+            string actualFullName = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(excpectedFullName, actualFullName);
+        }
+    }
+}
