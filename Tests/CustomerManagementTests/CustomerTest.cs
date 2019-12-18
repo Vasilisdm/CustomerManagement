4614a133e7631b3e734672e837e53ac7d227af7c
vasilisdm <vasilisdmr@gmail.com>
2019-12-18 23:16:15 +0200

    Refactored the tests in order to check for the lastname first.

--- Tests/CustomerManagementTests/CustomerTest.cs
+++ Tests/CustomerManagementTests/CustomerTest.cs
@@ -15,7 +15,43 @@ namespace CustomerManagementTests
                 LastName = "Baggins"
             };
 
-            string excpectedFullName = "Frodo Baggins";
+            string excpectedFullName = "Baggins, Frodo";
+
+            // Act
+            string actualFullName = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(excpectedFullName, actualFullName);
+        }
+
+        [Test]
+        public void FullNameTestEmptyFirstName()
+        {
+            // Arrange
+            Customer customer = new Customer()
+            {
+                LastName = "Baggins"
+            };
+
+            string excpectedFullName = "Baggins";
+
+            // Act
+            string actualFullName = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(excpectedFullName, actualFullName);
+        }
+
+        [Test]
+        public void FullNameTestEmptyLastName()
+        {
+            // Arrange
+            Customer customer = new Customer()
+            {
+                FirstName = "Frodo"
+            };
+
+            string excpectedFullName = "Frodo";
 
             // Act
             string actualFullName = customer.FullName;
