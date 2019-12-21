7fc55a24b23963f4c6f4cf14b0894a267e92016e
vasilisdm <vasilisdmr@gmail.com>
2019-12-21 16:49:05 +0200

    Created the dummy product repository.

---
+++ CustomerManagement/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CustomerManagement
+{
+    public class ProductRepository
+    {
+        public Product Retrieve(int productId)
+        {
+            // Dummy product retrieve
+            Product product = new Product(productId);
+
+            if (productId==2)
+            {
+                product.ProductName = "Sunflowers";
+                product.ProductDescription = "Assorted size set for 4 Bright Yellow Mini Sunflowers";
+                product.CurrentPrice = 15.96M;
+            }
+
+            return product;
+        }
+
+        public bool Save(Product product)
+        {
+            // Dummy product store.
+
+            return true;
+        }
+    }
+}
