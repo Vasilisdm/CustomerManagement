1174754a9264bd2b395a58fac9a0c38b843f865d
vasilisdm <vasilisdmr@gmail.com>
2019-12-21 11:17:00 +0200

    Added the validate method.

--- CustomerManagement/Customer.cs
+++ CustomerManagement/Customer.cs
@@ -24,5 +24,23 @@ namespace CustomerManagement
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(FirstName))
+            {
+                isValid = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
     }
 }
