9fc3dec6e1677662fcbd2b3b53489f80b74a73a6
vasilisdm <vasilisdmr@gmail.com>
2019-12-21 12:09:02 +0200

    Added the order class with the backbone of it's methods.

---
+++ CustomerManagement/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace CustomerManagement
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+
+        public Order(int orderId)
+        {
+
+        }
+
+        public int OrderId { get; private set; }
+        public DateTimeOffset? OrderDate { get; set; }
+
+        public Order Retrieve(int orderId)
+        {
+            // Retrieve provided order, needs implementation
+
+            return new Order();
+        }
+
+        public bool Save()
+        {
+            // save order, needs implementation
+
+            return true;
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+
+            if (OrderDate == null)
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
