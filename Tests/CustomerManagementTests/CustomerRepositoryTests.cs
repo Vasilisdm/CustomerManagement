d9af6773209761a3f31c093bb2c66672b3447dd8
vasilisdm <vasilisdmr@gmail.com>
2019-12-21 16:44:10 +0200

    Wrote a unit test wich check if the retrieved customer is equal to the initialized customer.

---
+++ Tests/CustomerManagementTests/CustomerRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using CustomerManagement;
+using NUnit.Framework;
+
+namespace CustomerManagementTests
+{
+    public class CustomerRepositoryTests
+    {
+        [Test]
+        public void RetrieveValid()
+        {
+            // Arrange
+            CustomerRepository customerRepo = new CustomerRepository();
+            Customer customer = new Customer(1)
+            {
+                Email = "Baggins@gmail.com",
+                FirstName = "Frodo",
+                LastName = "Baggins"
+            };
+
+            // Act
+            Customer actualCustomer = customerRepo.Retrieve(1);
+
+            // Assert
+            Assert.AreEqual(customer.Email, actualCustomer.Email);
+            Assert.AreEqual(customer.FirstName, actualCustomer.FirstName);
+            Assert.AreEqual(customer.LastName, actualCustomer.LastName);
+        }
+    }
+}
