fd9316ea7ec4e977c0285cf60273c84622d374c7
vasilisdm <vasilisdmr@gmail.com>
2019-12-21 12:18:20 +0200

    Added the OrderItem class alongside with it's backbone methods.

---
+++ CustomerManagement/OrderItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace CustomerManagement
+{
+    public class OrderItem
+    {
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int orderItemId)
+        {
+
+        }
+
+        public int OrderItemId { get; private set; }
+        public int ProductId { get; set; }
+        public decimal? PurchasePrice { get; set; }
+        public int Quantity { get; set; }
+
+        public OrderItem Retrieve(int orderItemId)
+        {
+            // Retrieve provided orderItemId, needs implementation
+
+            return new OrderItem();
+        }
+
+        public bool Save()
+        {
+            // Save orderItem
+
+            return true;
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+
+            if (Quantity<=0) isValid = false;
+
+            if (ProductId<=0) isValid = false;
+
+            if (PurchasePrice<=0) isValid = false;
+
+            return isValid;
+        }
+    }
+}
