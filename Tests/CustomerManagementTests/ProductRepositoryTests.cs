df4c74f022ed820d6729e124f05616f839480aaa
vasilisdm <vasilisdmr@gmail.com>
2019-12-21 16:55:56 +0200

    Unit test for the productRepository.

---
+++ Tests/CustomerManagementTests/ProductRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using CustomerManagement;
+using NUnit.Framework;
+namespace CustomerManagementTests
+{
+    public class ProductRepositoryTests
+    {
+        [Test]
+        public void ProductRetrieveTest()
+        {
+            // Arrange
+            ProductRepository productRepository = new ProductRepository();
+            Product product = new Product()
+            {
+                ProductName = "Sunflowers",
+                ProductDescription = "Assorted size set for 4 Bright Yellow Mini Sunflowers",
+                CurrentPrice = 15.96M
+            };
+
+            // Act
+            Product actualProduct = productRepository.Retrieve(2);
+
+            // Assert
+            Assert.AreEqual(product.ProductName, actualProduct.ProductName);
+            Assert.AreEqual(product.ProductDescription, actualProduct.ProductDescription);
+            Assert.AreEqual(product.CurrentPrice, actualProduct.CurrentPrice);
+        }
+    }
+}
