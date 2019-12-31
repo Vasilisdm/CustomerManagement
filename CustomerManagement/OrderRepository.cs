e00024645ba30bf6f98f2d1aa419068b2368f32b
vasilisdm <vasilisdmr@gmail.com>
2019-12-31 14:50:23 +0200

    Created the dummy OrderRepository.

---
+++ CustomerManagement/OrderRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CustomerManagement
+{
+    public class OrderRepository
+    {
+        public Order Retrieve(int orderId)
+        {
+            Order order = new Order(orderId);
+
+            if (orderId==10)
+            {
+                order.OrderDate = new DateTimeOffset(DateTime.Now.Year, 12, 31, 14, 45, 40, new TimeSpan(5, 0, 0));
+            }
+
+            return order;
+        }
+
+
+        public bool Save()
+        {
+            // dummy order save
+
+            return true;
+        } 
+    }
+}
