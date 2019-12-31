5b26ba86050377fbcb7f09b852f35e580660f531
vasilisdm <vasilisdmr@gmail.com>
2019-12-31 22:56:27 +0200

    Modified CustomerRepository to use AddressRepository.

--- CustomerManagement/CustomerRepository.cs
+++ CustomerManagement/CustomerRepository.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Linq;
+
 namespace CustomerManagement
 {
     public class CustomerRepository
     {
+        public CustomerRepository()
+        {
+            addressRepository = new AddressRepository();
+        }
+
+        private AddressRepository addressRepository { get; set; }
+
         public Customer Retrieve(int customerId)
         {
             // Dummy retrieve
@@ -13,6 +22,7 @@ namespace CustomerManagement
                 customer.FirstName = "Frodo";
                 customer.LastName = "Baggins";
                 customer.Email = "Baggins@gmail.com";
+                customer.AddressList = addressRepository.RetrieveByCustomerId(1).ToList();
             }
 
             return customer;
