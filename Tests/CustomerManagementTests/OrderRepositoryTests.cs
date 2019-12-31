c1f5d4a822c16ed3cc8acf0365eb6e57ded0532b
vasilisdm <vasilisdmr@gmail.com>
2019-12-31 15:04:50 +0200

    Added a test for retrieve method of OrederRepository.

---
+++ Tests/CustomerManagementTests/OrderRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CustomerManagement;
+using NUnit.Framework;
+
+namespace CustomerManagementTests
+{
+    public class OrderRepositoryTests
+    {
+        [Test]
+        public void OrderRetrieveTest()
+        {
+            // Arrange
+            OrderRepository orderRepository = new OrderRepository();
+            Order order = new Order(10)
+            {
+                OrderDate = new DateTimeOffset(DateTime.Now.Year, 12, 31, 14, 45, 40, new TimeSpan(5, 0, 0))
+            };
+
+            // Act
+            var actualOrder = orderRepository.Retrieve(10);
+
+            // Assert
+            Assert.AreEqual(order.OrderDate, actualOrder.OrderDate);
+        }
+    }
+}
