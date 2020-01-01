fede1f1af57a07be637dde173193c4ea8e423183
vasilisdm <vasilisdmr@gmail.com>
2020-01-01 17:11:20 +0200

    Added tests for both valid and invalid save!

--- Tests/CustomerManagementTests/ProductRepositoryTests.cs
+++ Tests/CustomerManagementTests/ProductRepositoryTests.cs
@@ -24,5 +24,45 @@ namespace CustomerManagementTests
             Assert.AreEqual(product.ProductDescription, actualProduct.ProductDescription);
             Assert.AreEqual(product.CurrentPrice, actualProduct.CurrentPrice);
         }
+
+        [Test]
+        public void SaveTestValid()
+        {
+            // Arrange
+            ProductRepository productRepository = new ProductRepository();
+            Product product = new Product()
+            {
+                ProductName = "Sunflowers",
+                ProductDescription = "Assorted size set for 4 Bright Yellow Mini Sunflowers",
+                CurrentPrice = 15.96M,
+                HasChanges = true
+            };
+
+            // Act
+            bool isProductSaved = productRepository.Save(product);
+
+            // Assert
+            Assert.AreEqual(true, isProductSaved);
+        }
+
+        [Test]
+        public void SaveTestInvalid()
+        {
+            // Arrange
+            ProductRepository productRepository = new ProductRepository();
+            Product product = new Product()
+            {
+                ProductName = "Sunflowers",
+                ProductDescription = "Assorted size set for 4 Bright Yellow Mini Sunflowers",
+                CurrentPrice = null,
+                HasChanges = true
+            };
+
+            // Act
+            bool isProductSaved = productRepository.Save(product);
+
+            // Assert
+            Assert.AreEqual(false, isProductSaved);
+        }
     }
 }
