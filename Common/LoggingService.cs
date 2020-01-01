67c716e995d4624418110a7987746f8cc277d38d
vasilisdm <vasilisdmr@gmail.com>
2020-01-01 22:34:46 +0200

    Added LoggingService static class, with WriteToFile method.

---
+++ Common/LoggingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public static class LoggingService
+    {
+        public static void WriteToFile(List<ILoggable> itemsToLog)
+        {
+            foreach (ILoggable item in itemsToLog)
+            {
+                Console.WriteLine(item.Log());
+            }
+        }
+    }
+}
