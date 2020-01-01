40c959594cff0276cd62d5ed923cdee3f42b3515
vasilisdm <vasilisdmr@gmail.com>
2020-01-01 16:38:43 +0200

    Created entity base class which is going to be the base class for the following functionality, isnew, haschanges, entitystate and isvalid.

---
+++ CustomerManagement/EntityBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace CustomerManagement
+{
+    public enum EntityStateOption
+    {
+        Active,
+        Delete
+    }
+
+    public abstract class EntityBase
+    {
+        public EntityStateOption EntityState { get; set; }
+        public bool IsNew
+        {
+            get;
+            private set;
+        }
+        public bool HasChanges { get; set; }
+        public bool IsValid
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
