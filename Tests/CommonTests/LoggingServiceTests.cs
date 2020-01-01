c27277a06257c763bb1bf4e4438721d759f1aa24
vasilisdm <vasilisdmr@gmail.com>
2020-01-01 23:50:50 +0200

    Dummy test for the logging service.

---
+++ Tests/CommonTests/LoggingServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using CustomerManagement;
+using NUnit.Framework;
+
+namespace CommonTests
+{
+    public class LoggingServiceTests
+    {
+        [Test]
+        public void WriteToFileTest()
+        {
+            // Arrange
+            List<ILoggable> changedItems = new List<ILoggable>();
+            
+            Customer customer = new Customer(1)
+            {
+                Email = "fbaggins@hobbiton.me",
+                FirstName = "Frodo",
+                LastName = "Baggins",
+                AddressList = null
+            };
+
+            changedItems.Add(customer);
+
+            Product product = new Product(2)
+            {
+                ProductName = "Rake",
+                ProductDescription = "Garden Rake with steel head",
+                CurrentPrice = 6M
+            };
+
+            changedItems.Add(product);
+
+            // Act
+            LoggingService.WriteToFile(changedItems);
+
+            // Assert
+        }
+
+    }
+}
