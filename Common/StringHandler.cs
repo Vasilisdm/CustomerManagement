323f7b41c8cf24525b700b140412c1c354b2b6e2
vasilisdm <vasilisdmr@gmail.com>
2020-01-01 20:25:32 +0200

    Added the common project which is going to contain  methods that are going to be used from different classes within the project.

---
+++ Common/StringHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Common
+{
+    public class StringHandler
+    {
+        public string InsertSpaces(string inputString)
+        {
+            string result = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(inputString))
+            {
+                foreach (char letter in inputString)
+                {
+                    if (char.IsUpper(letter))
+                    {
+                        result += " ";
+                    }
+                    result += letter;
+                }
+            }
+            return result;
+        }
+    }
+}
